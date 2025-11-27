ef92de99042a07f867efc84165dbf5c27f1f7376
Alice Emily <alice.nascimento@bsatech.io>
2025-11-27 11:08:56 -0300

    refactor: apaga schema que não está utilizado

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -2,10 +2,8 @@ from pydantic import BaseModel, Field
 from typing import Optional
 from datetime import datetime
 
+
 class CreateUser(BaseModel):
    id: Optional[str] = Field(default=None, description="Id gerado pelo mongodb")
    name: str
-   create_at: datetime
-
-class UserSchemaBody(BaseModel):
-   name: str
+   create_at: Optional[datetime] = None
