5e289177657664982df2c2da89fcfc60f0204629
Alice Vital <alice.nascimento@bsatech.io>
2025-11-27 14:31:32 -0300

    fix: corrige erro do campo created_at

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -6,4 +6,7 @@ from datetime import datetime
 class CreateUser(BaseModel):
    id: Optional[str] = Field(default=None, description="Id gerado pelo mongodb")
    name: str
-   create_at: Optional[datetime] = None
+   created_at: Optional[datetime] = None
+
+class UserRequest(BaseModel):
+   name: str
